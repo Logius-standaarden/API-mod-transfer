78a5a21abe04f8023bd5113285fd5f5d6ad63396
Alexander Green <alexander.green@logius.nl>
2026-09-04 16:55:55 +0200

    `server-to-client` metadata resource

--- examples/aspnet/Program.cs
+++ examples/aspnet/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var largeFiles = Path.Combine(AppContext.BaseDirectory, "large-files");
+
+app.MapGet("/server-to-client", () =>
+{
+    var identifiers = Directory.GetFiles(largeFiles, "*.txt").Select(Path.GetFileNameWithoutExtension);
+    return $"The following files are available for download:\n\n- {string.Join("\n- ", identifiers)}\n";
+});
+
+app.MapGet("/server-to-client/{fileIdentifier}", (string fileIdentifier) =>
+{
+    var identifier = Path.GetFileName(fileIdentifier);
+
+    var file = new FileInfo(Path.Combine(largeFiles, $"{identifier}.txt"));
+    if (!file.Exists)
+        return Results.Problem(title: "Could not obtain file", detail: "File does not exist",
+            statusCode: StatusCodes.Status404NotFound);
+
+    return Results.Ok(new MetadataResource(
+        file.Name,
+        "text/plain",
+        file.Length,
+        $"/server-to-client/{identifier}/content"));
+});
 
 app.Run();
